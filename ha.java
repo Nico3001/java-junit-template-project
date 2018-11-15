b2b909e7705c008cd7471072ac24676daee59682
Nicolas S <43515349+Nico3001@users.noreply.github.com>
2018-11-15 19:10:38 +0100

    Add files via upload
    Hausaufgabe ISE

---
+++ ha.java
@@ -0,0 +1,64 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class ha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ha test = new ha ();
+		
+		test.rechnung();
+	
+		
+		
+		
+	}
+
+	public static int fakul(int n ) {
+
+		int ergebniss = 1 ;
+		int i = 1 ;
+		while (i<=n) {
+			ergebniss = ergebniss * i;
+			i++ ;
+			
+		}
+		return ergebniss ;
+
+	}
+	public double rechnung () {
+		 
+		double Lösung  ;
+		 try {
+			Scanner Eingabe = new Scanner(System.in).useLocale(Locale.ENGLISH);
+			System.out.println("Wie groß soll n sein ?");
+	        int n = Eingabe.nextInt();
+			
+			System.out.println("Wie groß soll k sein ?") ;
+			int k = Eingabe.nextInt();
+			
+			int y = n-k ;
+			 Lösung = fakul(n) / (fakul(n-k)*fakul(k)) ;
+			
+			
+			System.out.println(Lösung);
+			 return  Lösung ;
+			
+		}
+		
+			
+			catch(Exception e) {
+				System.out.println("Fehlerhafte eingabe ");
+				double aLösung = 0 ;
+				Lösung = aLösung ;
+				return Lösung ;
+			}
+		
+			
+	}
+	
+}
+	
+
+
+
---
+++ haTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+class haTest {
+	
+	
+
+	@Test
+	void test() {
+		ha test = new ha();
+		
+		double n = test.fakul(3);
+		
+		assertEquals(6,n);
+
+		
+		
+		
+  }
+	
+	@Test
+	
+	/*Die System ins der Scanner (n,k) können bei test nicht simuliert werden
+	/Diese sind für das testen relevant */ 
+	
+	void test2() {
+		ha test2 = new ha();
+	
+		
+	 
+	 double r = test2.rechnung();
+	 
+		
+		
+		assertEquals(10,r );
+	}
+	
+	
+}
